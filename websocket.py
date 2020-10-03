a39b2538beadefb35466638bb16680fc7ce716f0
Peeradech Abhidechsakhulchai <p.abhidechsakhulchai@gmail.com>
2020-10-03 16:25:14 -0700

    second test

--- websocket.py
+++ websocket.py
@@ -1,7 +1,7 @@
 #import socket module
 from socket import *
 import sys # In order to terminate the program
-serverPort = 5678
+serverPort = 5577
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
@@ -15,7 +15,6 @@ while True:
     connectionSocket, addr = serverSocket.accept()#Fill in start #Fill in end
     try:
         message = connectionSocket.recv(1024)
-        print(message,'::', message.split()[0],':',message.split()[1])
         #Fill in start #Fill in end
         filename = message.split()[1]
         f = open(filename[1:])
@@ -24,18 +23,18 @@ while True:
         print(outputdata)
         #Send one HTTP header line into socket
         #Fill in start
-        connectionSocket.send('\nHTTP/1.1 200 OK\n\n')
-        connectionSocket.send(outputdata)
+        connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
+        # connectionSocket.send(outputdata)
         #Fill in end
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode()) #'utf-8'
-            connectionSocket.send("\r\n".encode())
-            connectionSocket.close()
+                connectionSocket.send(outputdata[i].encode()) 
+                connectionSocket.send("\r\n".encode())
+                connectionSocket.close()
     except IOError:
         #Send response message for file not found
         #Fill in start 
-        connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n'.encode())
+        connectionSocket.send("\nHTTP/1.1 404 Not Found\n\n".encode())
         #Fill in end
         #Close client socket
         #Fill in start
